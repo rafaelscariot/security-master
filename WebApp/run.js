81bbdc866449ef246d66f1e97b2e2b1ac4f369c7
Rafael Scariot <rafael.scariot@pampa.compasso>
2021-09-05 21:03:10 -0300

    rename folders

--- WebApp/run.js
+++
@@ -1,6 +0,0 @@
-const app = require('./src/config/custom-express');
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log('[INFO] Server is running on port', PORT);
-});
--- WebApp/src/app/views/base/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    error404: require('./errors/404.marko'),
-    error500: require('./errors/500.marko'),
-    home: require('./home/home.marko')
-}
