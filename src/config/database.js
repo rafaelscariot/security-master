be5f830829b7c64aeba20806e52519f72209e65e
rafaelscariot <rafael.scariot.ext@dasa.com.br>
2021-12-26 14:39:45 -0300

    front update

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/security_master", {
+mongoose.connect("mongodb://mongodb/security_master", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
